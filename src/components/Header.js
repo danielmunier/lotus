ac13602239e8251b9a547c79ff46f1c1faacc66e
Daniel <dmunier27@gmail.com>
2022-11-24 00:46:19 -0300

    test on production yes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,18 @@ import Rules from '../pages/Rules'
 
 function Header() {
     return (
-            <Router>
+       
         <header>
-        <Link to='/'><img src={DiscordLogo}/></Link>
+   
             <nav>
 
                 <ul>
 
                     
-                    <Link to="/about">Sobre o servidor</Link>
                     <a href="https://discord.gg/278nTuHQ" target="_blank">Comunidade</a>
                     <a href="https://discord.gg/278nTuHQ" target="_blank">Membros</a>
-                    <Link to="/rules">Regras</Link>
+                    <a href="https://discord.gg/278nTuHQ" target="_blank">Membros</a>
+                    <a href="https://discord.gg/278nTuHQ" target="_blank">Membros</a>
 
             
                 </ul>
@@ -27,12 +27,7 @@ function Header() {
 
             </nav>
         </header>
-                <Routes>
-                    <Route path='/' element={<Home />}></Route>
-                    <Route path='/about' element={<About />}> </Route>
-                    <Route path='/rules' element={<Rules />}> </Route>
-                </Routes>
-            </Router>
+    
             
     )
 }
